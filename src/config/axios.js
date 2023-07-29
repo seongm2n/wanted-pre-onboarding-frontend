9633fb14f2ddd652c5ebe66e0885c71deee59824
seongmin <seongm22n@gmail.com>
2023-07-30 02:41:39 +0900

    CHORE: 초기 세팅

---
+++ src/config/axios.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { REACT_APP_API_URI } from '../env.config';
+
+const getAxiosInstance = (contentType) => {
+  const config = {
+    baseURL: REACT_APP_API_URI,
+    headers: {
+      'Access-Control-All-Origin': '*',
+      'Content-Type': contentType,
+    },
+  };
+  const instance = axios.create(config);
+  instance.defaults.timeout = 12000;
+
+  return instance;
+};
+
+export const axiosJsonInstance = getAxiosInstance('application.json');
