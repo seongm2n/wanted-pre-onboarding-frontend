9c5ab9e00d31fab2f0763c8e7262c5ffef7f5f5f
seongmin <seongm22n@gmail.com>
2023-08-01 21:32:36 +0900

    FIX : Todo 페이지 수정

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import TodoList from '../components/Todo/TodoList';
 
 function Todo() {
-  return <div>Todos</div>;
+  return (
+    <div className="w-full flex flex-col m-6 p-1 items-center ">
+      <h1>오늘 나의 할일</h1>
+      <TodoList />
+    </div>
+  );
 }
 export default Todo;
