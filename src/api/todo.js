381004a5967f9389dfd88444405c6c9dfc5bdc68
seongmin <seongm22n@gmail.com>
2023-08-01 21:31:38 +0900

    FEAT: todo api 생성

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,69 +1,83 @@
 import { axiosJsonInstance } from '../config/axios';
 
 //getTodos
-export const getTodos = async (access_token) => {
+export const getTodos = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const header = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
   try {
-    const { data } = await axiosJsonInstance.get(`/todos`, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
+    const { data } = await axiosJsonInstance.get('/todos', header);
     return { data };
   } catch (error) {
-    throw new Error('Error get todos: ', error.message);
+    throw new Error(error.message);
   }
 };
 
 //createTodo
-export const createTodo = async (access_token, todo) => {
+export const createTodo = async (todo) => {
+  const accessToken = localStorage.getItem('access_token');
+  const header = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+  const payload = {
+    todo: todo,
+  };
+
   try {
-    const body = {
-      todo,
-    };
-    const { data } = await axiosJsonInstance.post(`/todos`, body, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
+    const { data } = await axiosJsonInstance.post('/todos', payload, header);
     return { data };
   } catch (error) {
-    throw new Error('Error creating todo: ', error.message);
+    throw new Error(error.message);
   }
 };
 
 //updateTodo
-export const updateTodo = async (access_token, todoId, todo, isCompleted) => {
+export const updateTodo = async (id, todo, isCompleted) => {
+  const accessToken = localStorage.getItem('access_token');
+  const header = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+  const payload = {
+    todo,
+    isCompleted,
+  };
   try {
-    const body = {
-      todo,
-      isCompleted,
-    };
-    const { data } = await axiosJsonInstance.put(`/todos/${todoId}`, body, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
-    return { data };
+    const { data } = await axiosJsonInstance.put(
+      `/todos/${id}`,
+      payload,
+      header
+    );
+    return data;
   } catch (error) {
-    throw new Error('Error updating todo: ', error.message);
+    throw new Error(error.message);
   }
 };
 
 //deleteTodo
-export const deleteTodo = async (access_token, todoId) => {
+export const deleteTodo = async (id) => {
+  const accessToken = localStorage.getItem('access_token');
+  const header = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
   try {
-    const response = await axiosJsonInstance.delete(`/todos/${todoId}`, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
+    const response = await axiosJsonInstance.delete(`/todos/${id}`, header);
 
     if (response.status === 204) {
       return;
     } else {
-      throw new Error('Error deleting todo: ', response.status);
+      throw new Error(response.status);
     }
   } catch (error) {
-    throw new Error('Error deleting todo: ', error.message);
+    throw new Error(error.message);
   }
 };
