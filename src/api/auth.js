075e9bd597f259a800b29a4f1253877ddda94ccc
seongmin <seongm22n@gmail.com>
2023-08-03 22:08:16 +0900

    FIX : signup 코드 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -25,8 +25,7 @@ export const signUp = async ({ email, password }) => {
 			password: password,
 		};
 		const { data } = await axiosJsonInstance.post(`/auth/signup`, payload);
-		const accessToken = data.access_token;
-		localStorage.setItem('access_token', accessToken);
+
 		return { data };
 	} catch (error) {
 		throw new Error(error.response.data.message);
