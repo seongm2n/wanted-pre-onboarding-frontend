8de0806f844ba8a178781a802f603db06a8ab5ac
seongmin <seongm22n@gmail.com>
2023-08-03 20:55:47 +0900

    FEAT : notfound 생성

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 function NotFound() {
-  return <div>Error</div>;
+	return (
+		<div className='flex flex-col justify-center items-center w-full'>
+			<img
+				src='../images/404.gif'
+				alt='notfound'
+			/>
+		</div>
+	);
 }
 export default NotFound;
