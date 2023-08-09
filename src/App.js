605c5b39e669a901f939255d10cc805b263e5d31
seongmin <seongm22n@gmail.com>
2023-08-09 11:23:47 +0900

    FEAT : 애플리케이션을 감싸는TodoProvider사용하여 Context를 제공

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import { TodoProvider } from './context/TodoContext';
 
 function App() {
 	return (
-		<>
+		<TodoProvider>
 			<Header />
 			<Outlet />
-		</>
+		</TodoProvider>
 	);
 }
 
