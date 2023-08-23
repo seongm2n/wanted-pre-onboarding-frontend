251663b5ae4235697d2a68cbadc6420c0ceb7cde
seongmin <seongm22n@gmail.com>
2023-08-24 00:41:31 +0900

    FIX : routes 폴더로 이동

---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+export const ProtectedRoute = ({ children }) => {
+	const accessToken = localStorage.getItem('access_token');
+
+	if (!accessToken) {
+		return (
+			<Navigate
+				to='/signin'
+				replace
+			/>
+		);
+	}
+
+	return children;
+};
